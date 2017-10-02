dae64c050ccb52a0adb38f6c723ba25e3f51447c
23BFF0N\DXenu <talaikis.tadas@gmail.com>
2017-10-02 11:24:24 +0300

    initial clone

---
+++ README.md
@@ -0,0 +1,30 @@
+# BulmaPGV
+
+BulmaPGV is a 3-part website boilerplate:
+ 
+* Python/ Django acting as a content management backend,
+* Golang as an API and
+* NuxtJs/ VueJs 2 as a frontend.
+
+## Demo(s) in play
+
+* ...
+
+## UI(s)
+
+This version uses Bulma for UI.
+
+* for Bootstrap 4 version see [pygovue](https://github.com/xenu256/pygovue)
+* for Vuetify [tipgv](https://github.com/xenu256/tipgv).
+* for iView UI: [elpgv](https://github.com/xenu256/elpgv)
+
+## Requirements
+
+Python 3.6, Golang 1.8, Node 6.10 or 8.6, Postgres.
+
+## Setup
+
+1. Create database.
+2. Edit .env
+3. Create some content (Django admin or programmatically).
+3. Build with build.sh (aproximately)
---
+++ frontend/components/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+    <footer>
+        <Row type="flex">
+            <Col :span="3"></Col>
+            <Col :span="6" class="align-center">
+                <div>
+                    <a :href="baseUrl"><img src="~/assets/logo/logo.png" width="50" height="50" :alt="siteName"></a>
+                </div>
+                <div>
+                    <p><a :href="'https://twitter.com/' + twHandle"><i class="fa fa-twitter"></i></a>&nbsp;
+                    <a :href="'https://www.facebook.com/' + fbHandle"><i class="fa fa-facebook"></i></a></p>
+                    <address>
+                        <p>&copy; 2017 <a :href="baseUrl">{{ siteName }}</a> | 
+                        Developed by <a href="https://talaikis.com">Talaikis Inc.</a></p>
+                    </address>					
+                </div>
+            </Col>
+            <Col :span="3"></Col>
+        </Row>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'footerComponent',
+  data () {
+    return {
+      baseUrl: process.env.BASE_URL,
+      twHandle: process.env.TWITTE_HANDLE,
+      fbHandle: process.env.FACEBOOK_HANDLE,
+      siteName: process.env.SITE_NAME
+    }
+  }
+}
+</script>
+
+<style>
+.align-center {
+    text-align: center;
+    font-size: 0.8em;
+    padding: 5em 0 5em 0;
+    color: #9ea7b4;
+  }
+</style>
